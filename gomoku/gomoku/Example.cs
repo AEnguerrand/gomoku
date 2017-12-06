b24b8a2b918a8604db1f525221b6a462708fdcf8
Maxime Cauvin <maxime.cauvin@epitech.eu>
2017-12-06 14:11:04 +0100

    AiImprovement(feat): AI reworked, need to implement an recursive prediction

--- gomoku/gomoku/Example.cs
+++ gomoku/gomoku/Example.cs
@@ -119,7 +119,7 @@ class GomocupEngine : GomocupInterface
         return 2;
     }
 
-    void resetScoreBoard()
+    void ResetScoreBoard()
     {
         for (int i = 0; i < scoreBoard.Count; ++i)
         {
@@ -128,154 +128,83 @@ class GomocupEngine : GomocupInterface
         }
     }
 
-    int checkDirection(int posX, int posY, int xDir, int yDir, int baseScoreMe, int baseScoreOp)
+    int CheckDirection(int posX, int posY, int xDir, int yDir, char player)
     {
-        if (posY < 0 && posY >= board.Count)
-            return (0);
-        int scoreMe = baseScoreMe;
-        int scoreOp = baseScoreOp;
-        bool stopMe = false;
-        bool stopOp = false;
-        int loop = 0;
-        short foundMe = 0;
-        short foundOp = 0;
-        for (int i = posY, j = posX; loop < 5 && (!stopMe || !stopOp) && i >= 0 && i < board.Count && j >= 0 && j < board[i].Count; i += yDir, j += xDir)
+        string s = "X";
+        bool blockCursor1 = false;
+        bool blockCursor2 = false;
+        for (int loop = 0, x1 = posX + xDir, y1 = posY + yDir, x2 = posX + xDir * -1, y2 = posY + yDir * -1;
+             loop < 5;
+             loop++, x1 += xDir, x2 += xDir * -1, y1 += yDir, y2 += yDir * -1)
         {
-            if (board[i][j] == '0' && loop == 4)
+            if (!blockCursor1)
             {
-                if (!stopMe && foundMe == 3)
-                    scoreMe += 20;
-                if (!stopOp && foundOp == 3)
-                    scoreOp += 20;
+                if (y1 < 0 || y1 >= board.Count || x1 < 0 || x1 >= board[y1].Count)
+                    blockCursor1 = true;
+                else if (board[y1][x1] == player)
+                    s = "X" + s;
+                else if (board[y1][x1] == '0')
+                    s = "H" + s;
+                else
+                    blockCursor1 = true;
             }
-            else if (board[i][j] == '1')
+            if (!blockCursor2)
             {
-                if (!stopMe)
-                {
-                    foundMe += 1;
-                    scoreMe += 10 * foundMe;
-                    stopOp = true;
-                }
-                scoreOp -= 20;
+                if (y2 < 0 || y2 >= board.Count || x2 < 0 || x2 >= board[y2].Count)
+                    blockCursor2 = true;
+                else if (board[y2][x2] == player)
+                    s += "X";
+                else if (board[y2][x2] == '0')
+                    s += "H";
+                else
+                    blockCursor2 = true;
             }
-            else if (board[i][j] == '2')
-            {
-                if (!stopOp)
-                {
-                    foundOp += 1;
-                    scoreOp += 10 * foundOp;
-                    stopMe = true;
-                }
-                scoreMe -= 20;
-            }
-            loop += 1;
-            if (i + yDir < 0 || i + yDir >= board.Count || j + xDir < 0 && j + xDir >= board[i].Count)
-                return (0);
         }
-        if (foundMe == 4)
-            return (scoreMe * 200);
-        else if (foundOp == 4)
-            return (scoreOp * 100);
-        return ((scoreMe >= scoreOp) ? (scoreMe) : (scoreOp));
+        Console.WriteLine("DEBUG DIR: [{0}, {1}] | PLAYER: [{2}] | STRING: [{3}]", xDir, yDir, player, s);
+        if (s.Contains("XXXXX"))
+            return (41000000);
+        else if (s.Contains("HXXXXH"))
+            return (10100000);
+        else if (s.Contains("XXXXH") || s.Contains("HXXXX") || s.Contains("HXXXHH") || s.Contains("HHXXXH") || s.Contains("HXHXXH") ||
+            s.Contains("HXXHXH") || s.Contains("HXXXHX") || s.Contains("XHXXXH"))
+            return (2510000);
+        else if (s.Contains("XXXHH") || s.Contains("HHXXX") || s.Contains("XXHXH") || s.Contains("HXHXX") || s.Contains("XHXXH") ||
+                 s.Contains("HXXHX") || s.Contains("XXHHX") || s.Contains("XHHXX") || s.Contains("XHXHX"))
+            return (625100);
+        else if (s.Contains("XX"))
+            return (156260);
+        return (0);
     }
 
-    void CheckWeightAroundCell(int x, int y, out int baseScoreMe, out int baseScoreOp)
+    int CheckAllDirections(int x, int y)
     {
-        baseScoreMe = 0;
-        baseScoreOp = 0;
-        
-        //E
-        if (x + 1 < board[y].Count && board[y][x + 1] == '1')
-            baseScoreMe += 1;
-        else if (x + 1 < board[y].Count && board[y][x + 1] == '2')
-            baseScoreOp += 1;
-
-        //W
-        if (x - 1 >= 0 && board[y][x - 1] == '1')
-            baseScoreMe += 1;
-        else if (x - 1 >= 0 && board[y][x - 1] == '2')
-            baseScoreOp += 1;
-
-        //S
-        if (y + 1 < board.Count && board[y + 1][x] == '1')
-            baseScoreMe += 1;
-        else if (y + 1 < board.Count && board[y + 1][x] == '2')
-            baseScoreOp += 1;
-
-        //N
-        if (y - 1 >= 0 && board[y - 1][x] == '1')
-            baseScoreMe += 1;
-        else if (y - 1 >= 0 && board[y - 1][x] == '2')
-            baseScoreOp += 1;
-
-        //SE
-        if (y + 1 < board.Count && x + 1 < board[y].Count && board[y + 1][x + 1] == '1')
-            baseScoreMe += 1;
-        else if (y + 1 < board.Count && x + 1 < board[y].Count && board[y + 1][x + 1] == '2')
-            baseScoreOp += 1;
-
-        //NW
-        if (y - 1 >= 0 && x - 1 >= 0 && board[y - 1][x - 1] == '1')
-            baseScoreMe += 1;
-        else if (y - 1 >= 0 && x - 1 >= 0 && board[y - 1][x - 1] == '2')
-            baseScoreOp += 1;
-
-        //NE
-        if (y - 1 >= 0 && x + 1 < board[y].Count && board[y - 1][x + 1] == '1')
-            baseScoreMe += 1;
-        else if (y - 1 >= 0 && x + 1 < board[y].Count && board[y - 1][x + 1] == '2')
-            baseScoreOp += 1;
-
-        //SW
-        if (y + 1 < board.Count && x - 1 >= 0 && board[y + 1][x - 1] == '1')
-            baseScoreMe += 1;
-        else if (y + 1 < board.Count && x - 1 >= 0 && board[y + 1][x - 1] == '2')
-            baseScoreOp += 1;
+        List<int> weightsMe = new List<int>
+        {
+            CheckDirection(x, y, 0, 1, '1'),
+            CheckDirection(x, y, 1, 0, '1'),
+            CheckDirection(x, y, 1, 1, '1'),
+            CheckDirection(x, y, -1, 1, '1')
+        };
+        List<int> weightsOp = new List<int>
+        {
+            CheckDirection(x, y, 0, 1, '2'),
+            CheckDirection(x, y, 1, 0, '2'),
+            CheckDirection(x, y, 1, 1, '2'),
+            CheckDirection(x, y, -1, 1, '2')
+        };
+        Console.WriteLine("DEBUG [{0}, {1}]", x, y);
+        Console.WriteLine("DEBUG weightsMe: {0}", JsonConvert.SerializeObject(weightsMe));
+        Console.WriteLine("DEBUG weightsOp: {0}", JsonConvert.SerializeObject(weightsOp));
+        int sumMe = weightsMe.Sum();
+        int sumOp = weightsOp.Sum();
+        Console.WriteLine("DEBUG sumMe: {0} | sumOp {1}", sumMe, sumOp);
+        Console.WriteLine("DEBUG ------------------------------");
+        return ((sumMe >= sumOp) ? (sumMe) : (sumOp));
     }
 
-    List<int> checkAllDirections(int x, int y)
+    Point FindBestMove()
     {
-        List<int> list = new List<int>();
-        CheckWeightAroundCell(x, y, out int baseScoreMe, out int baseScoreOp);
-
-        // E
-        list.Add(checkDirection(x - 1, y, 1, 0, baseScoreMe, baseScoreOp));
-        list.Add(checkDirection(x, y, 1, 0, baseScoreMe, baseScoreOp));
-        // W
-        list.Add(checkDirection(x + 2, y, -1, 0, baseScoreMe, baseScoreOp));
-        list.Add(checkDirection(x + 1, y, -1, 0, baseScoreMe, baseScoreOp));
-        list.Add(checkDirection(x, y, -1, 0, baseScoreMe, baseScoreOp));
-        // S
-        list.Add(checkDirection(x, y - 1, 0, 1, baseScoreMe, baseScoreOp));
-        list.Add(checkDirection(x, y, 0, 1, baseScoreMe, baseScoreOp));
-        // N
-        list.Add(checkDirection(x, y + 2, 0, -1, baseScoreMe, baseScoreOp));
-        list.Add(checkDirection(x, y + 1, 0, -1, baseScoreMe, baseScoreOp));
-        list.Add(checkDirection(x, y, 0, -1, baseScoreMe, baseScoreOp));
-        // SE
-        list.Add(checkDirection(x - 1, y - 1, 1, 1, baseScoreMe, baseScoreOp));
-        list.Add(checkDirection(x, y, 1, 1, baseScoreMe, baseScoreOp));
-        // NW
-        list.Add(checkDirection(x + 2, y + 2, -1, -1, baseScoreMe, baseScoreOp));
-        list.Add(checkDirection(x + 1, y + 1, -1, -1, baseScoreMe, baseScoreOp));
-        list.Add(checkDirection(x, y, -1, -1, baseScoreMe, baseScoreOp));
-        // SW
-        list.Add(checkDirection(x + 1, y - 1, -1, 1, baseScoreMe, baseScoreOp));
-        list.Add(checkDirection(x, y, -1, 1, baseScoreMe, baseScoreOp));
-        // NE
-        list.Add(checkDirection(x - 2, y + 2, 1, -1, baseScoreMe, baseScoreOp));
-        list.Add(checkDirection(x - 1, y + 1, 1, -1, baseScoreMe, baseScoreOp));
-        list.Add(checkDirection(x, y, 1, -1, baseScoreMe, baseScoreOp));
-        Console.WriteLine("DEBUG [{0}, {1}] => E-1:{2}, E:{3}, W+2:{4}, W+1:{5}, W:{6}, S-1:{7}, S:{8}, N+2:{9}, N-1:{10}, N:{11}, SE1:{12}, SE:{13}, NW2:{14}, NW1:{15}, NW:{16}, SW1:{17}, SW:{18}, NE2:{19}, NE1:{20}, NE:{21}",
-                          x, y, list[0], list[1], list[2], list[3], list[4], list[5], list[6], list[0], list[7], list[8], list[9], list[10], list[11], list[12], list[13], list[14], list[15], list[16], list[17], list[18]);
-        return (list);
-    }
-
-    Point findBestMove()
-    {
-        Console.WriteLine("DEBUG debut findbestmove");
         int highScore = 0;
-        int max;
         Point bestPos = new Point((ushort)(height / 2), (ushort)(width / 2));
 
         for (int i = 0; i < board.Count; ++i)
@@ -284,15 +213,7 @@ class GomocupEngine : GomocupInterface
             {
                 if (board[i][j] == '0')
                 {
-                    List<int> weights = checkAllDirections(j, i);
-                    max = 0;
-                    for (int k = 0; k < weights.Count; k++)
-                    {
-                        if (max < weights[k])
-                            max = weights[k];
-                    }
-                    Console.WriteLine("DEBUG [{0}, {1}] => {2}", j, i, max);
-                    scoreBoard[i][j] = max;
+                    scoreBoard[i][j] = CheckAllDirections(j, i);
                     if (highScore < scoreBoard[i][j])
                     {
                         highScore = scoreBoard[i][j];
@@ -317,9 +238,9 @@ class GomocupEngine : GomocupInterface
             }
             Console.WriteLine("DEBUG {0}", s);
         }
-        resetScoreBoard();
+        ResetScoreBoard();
         Console.WriteLine("DEBUG avant findbestmove");
-        Point pos = findBestMove();
+        Point pos = FindBestMove();
         Console.WriteLine("DEBUG apres findbestmove");
         Console.WriteLine("DEBUG [{0}, {1}]", pos.X, pos.Y);
         do_mymove(pos.X, pos.Y);
